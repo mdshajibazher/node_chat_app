4470562393c51ada6296947b2c81d2a00ee10171
Md Shajib Azher <32217069+mdshajibazher@users.noreply.github.com>
2022-03-06 07:34:13 +0600

    image upload success

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,5 +1,37 @@
 function getUsers(req,res,next){
     res.render('users')
 }
+async function addUser(req,res,next){
+    let newUser;
+    const hashedPasssword = await bcrypt.hash(req.body.password,10);
+    if(req.files && req.files.length > 0){
+        newUser = new User({
+            ...req.body,
+            avatar: req.files[0].filename,
+            password: hashedPasssword,
+        })
+    }else{
+        newUser = new User({
+            ...req.body,
+            password: hashedPasssword,
+        })
+    }
 
-module.exports = {getUsers};
+    try{
+        const result = await newUser.save();
+        res.status(200).json({
+            message: 'user added successfully'
+        })
+    }catch{
+        res.status(500).json({
+            errors: {
+                common: {
+                    msg: "unknown error occured "
+                }
+            }
+        })
+    }
+
+
+}
+module.exports = {getUsers,addUser};
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {getUsers} = require('../controller/usersController');
+const {getUsers,addUser} = require('../controller/usersController');
 const decorateHtmlResponse = require('../middleware/common/decorateHtmlResponse');
+const avatarUpload = require('../middleware/users/avatarUpload');
+const {addUserValidators, addValidatorHandler} = require("../middleware/users/usersValidators");
 router.get('/',decorateHtmlResponse('users'),getUsers);
+router.post('/',avatarUpload,addUserValidators,addValidatorHandler,addUser);
+
+
 
 module.exports = router;
